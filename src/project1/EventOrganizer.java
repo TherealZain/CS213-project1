54b4dac5b20650e954667d9b740d4541d5e0daa7
nickyim <nicholasjyim@gmail.com>
2023-09-24 19:49:13 -0400

    change

--- src/project1/EventOrganizer.java
+++ src/project1/EventOrganizer.java
@@ -1,9 +1,11 @@
 package project1;
 
 import java.util.Scanner;
+import java.util.StringTokenizer;
 
 public class EventOrganizer {
     private boolean isRunning;
+    private static final int A_COMMAND_TOKENS = 7;
 
     public EventOrganizer() {
         isRunning = true;
@@ -16,8 +18,11 @@ public class EventOrganizer {
         while (isRunning) {
             String command = scanner.nextLine();
 
+            StringTokenizer tokenizer = new StringTokenizer(command);
+
             switch (command) {
                 case "Q" -> isRunning = false;
+                case "A" -> handleACommand(tokenizer);
                 case "R" -> handleRCommand();
                 case "P" -> handlePCommand();
                 case "PE" -> handlePECommand();
@@ -30,6 +35,18 @@ public class EventOrganizer {
         scanner.close();
     }
 
+    private void handleACommand(StringTokenizer tokenizer) {
+        if (tokenizer.countTokens() == A_COMMAND_TOKENS) {
+            String date = tokenizer.nextToken();
+            String startTime = tokenizer.nextToken();
+            String location = tokenizer.nextToken();
+            String department = tokenizer.nextToken();
+            String email = tokenizer.nextToken();
+            int duration = Integer.parseInt(tokenizer.nextToken());
+
+            Event event = new Event(date, startTime, location, contact, duration);
+        }
+    }
     private void handleRCommand() {
     }
 
